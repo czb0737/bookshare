0c145f7043239725131f6a4bf441ed2ffcfc974a
vincween <vincween.@gmail.com>
2013-12-07 17:33:46 +0800

    the last version

--- JsFuntion.js
+++ JsFuntion.js
@@ -14,3 +14,17 @@ function Search()
 {
    alert("Hello World!");
 }
+function changemodel (curnode,show,hide) 
+{
+	document.getElementById(show).style.display = "block";
+	curnode.style.color = "#2e5306";
+	document.getElementById(hide).style.display = "none";
+	if (curnode.id == "infoone") 
+	{
+		document.getElementById('infotwo').style.color = "#80662b";
+	}
+	else
+	{
+		document.getElementById('infoone').style.color = "#80662b";
+	}
+}
